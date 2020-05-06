805f350fd38aa6b3f6c6e073651ce0265e71926c
gmcmanus <gmcmanus@users.noreply.github.com>
2020-05-06 13:45:44 +1000

    Run backup after Anki backup completes
    The media sync runs on a timer and it's not good to reopen the
    collection after this (references to the old collection still exist).

--- backup/__init__.py
+++ backup/__init__.py
@@ -3,24 +3,24 @@ from anki.exporting import AnkiCollectionPackageExporter
 
 
 def main() -> None:
-    gui_hooks.media_sync_did_start_or_stop.append(backup)
+    gui_hooks.backup_did_complete.append(backup)
 
 
-def backup(media_sync_running: bool) -> None:
-    if media_sync_running:
-        return
-
+def backup() -> None:
     config = mw.addonManager.getConfig(__name__)
     backup_path = config['backup_path']
 
     if backup_path is None:
         return
 
+    # the collection is closed before the backup_did_complete hook, so reopen it
+    mw.reopen()
+
     exporter = AnkiCollectionPackageExporter(mw.col)
     exporter.exportInto(backup_path)
 
-    # exportInto closes the collection, so we need to reopen it
-    mw.reopen()
+    # exportInto closes the collection again so we are done
+
 
 
 main()
