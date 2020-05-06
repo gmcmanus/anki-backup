b6f152652210026b4a6b3432adca334624d7fcbe
gmcmanus <gmcmanus@users.noreply.github.com>
2020-05-06 14:08:46 +1000

    Allow the profile name to be included in the backup path
    Otherwise you can clobber your backup when you switch profiles.

--- backup/__init__.py
+++ backup/__init__.py
@@ -8,10 +8,12 @@ def main() -> None:
 
 def backup() -> None:
     config = mw.addonManager.getConfig(__name__)
-    backup_path = config['backup_path']
+    backup_path_template = config['backup_path']
 
-    if backup_path is None:
+    if backup_path_template is None:
         return
+
+    backup_path = backup_path_template.format(profile_name=mw.pm.name)
 
     # the collection is closed before the backup_did_complete hook, so reopen it
     mw.reopen()
--- backup/config.md
+++ backup/config.md
@@ -1,1 +1,2 @@
 `backup_path` is the path to do a full backup to. If it is null, no backup is performed.
+The string `{profile_name}` in the backup path is replaced by the profile name.
